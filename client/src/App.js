e1d98e9cabb3ca71f252bf8169819424e34ed89e
Bruno Rodrigues <lpbruno12@gmail.com>
2019-10-04 00:11:07 -0300

    convert code to be mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+
+import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
@@ -22,14 +22,15 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/signin' render={() =>
-          currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-      </Switch>
+      <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/signin' render={() =>
+            currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+        </Switch>
     </div>
   );
 }
